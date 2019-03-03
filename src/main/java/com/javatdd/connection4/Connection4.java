9fa5be99c631aee486c34dc0a2c3570430af2f7a
Ewelina <ewelina@Ewelinas-MacBook-Pro.local>
2019-03-03 17:16:30 +0100

    test&code added

--- src/main/java/com/javatdd/connection4/Connection4.java
+++ src/main/java/com/javatdd/connection4/Connection4.java
@@ -1,8 +1,45 @@
 package com.javatdd.connection4;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Connection4 {
 
+    private static int ROWS = 6;
+    private static int COLUMNS = 7;
+    private static final String PUSTE = " ";
+
+    private String[][] board = new String[ROWS][COLUMNS];
+
+    public Connection4(){
+        for (String[]row : board){
+            Arrays.fill(row, PUSTE);
+        }
+    }
+
     public int getNumberOfDiscs(){
-        return 0;
+        return IntStream.range(0, COLUMNS).map(this::getNumberOfDiscInColumn).sum();
+    }
+
+    private  int getNumberOfDiscInColumn(int column){
+        return  (int) IntStream.range(0, ROWS).filter(row -> !PUSTE.equals(board[row][column])).count();
+    }
+
+    public int putDiscInColumn(int column){
+        checkColumn(column);
+        int row = getNumberOfDiscInColumn(column);
+        checkPositionTOInsert(row, column);
+        board[row][column] = "X";
+        return row;
+    }
+
+    private void checkColumn(int column){
+        if(column < 0 || column >= COLUMNS)
+        throw new RuntimeException("Nieprawidłowa kolumna" + column);
+    }
+
+    private void checkPositionTOInsert(int row, int column){
+        if (row == ROWS)
+            throw new RuntimeException("Brak miejsca w kolumnie nr " + column);
     }
 }
--- src/test/java/com/javatdd/connection4/Connection4Spec.java
+++ src/test/java/com/javatdd/connection4/Connection4Spec.java
@@ -1,7 +1,9 @@
 package com.javatdd.connection4;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -10,13 +12,60 @@ public class Connection4Spec {
 
     private Connection4 tested;
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Before
     public void beforeEachTest(){
         tested = new Connection4();
     }
 
     @Test
-    public void whenTheGameIsStartedTheBoardIsEmty(){
+    public void whenTheGameIsStartedTheBoardIsEmpty(){
         assertThat(tested.getNumberOfDiscs(), is(0));
     }
+
+    @Test
+    public void whenDiscOutsideThenRuntimeException(){
+        int column = -1;
+        exception.expect(RuntimeException.class);
+        exception.expectMessage("Nieprawidłowa kolumna" + column);
+        tested.putDiscInColumn(column);
+    }
+
+    @Test
+    public void whenFirstDiscInColumnThenPositionIsZero(){
+        int column = 1;
+        assertThat(tested.putDiscInColumn(column), is(0));
+    }
+
+    @Test
+    public void whenSecondDiscInColumnThenPositionISOne(){
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.putDiscInColumn(column), is(1));
+    }
+
+    @Test
+    public void whenDiscInsertedThenNumberOfDiscIncreases(){
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.getNumberOfDiscs(), is(1));
+    }
+
+    @Test
+    public void whenNoMoreRoomInColumnThenRunTimeException(){
+        int column = 1;
+        int maxDiscsInColumn = 6;
+        for (int times = 0; times < maxDiscsInColumn; times++){
+            tested.putDiscInColumn(column);
+        }
+        exception.expect(RuntimeException.class);
+        exception.expectMessage("Brak miejsca w kolumnie nr " + column);
+        tested.putDiscInColumn(column);
+    }
+
+
+
+
 }
